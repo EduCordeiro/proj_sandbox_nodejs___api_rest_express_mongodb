ead46327f6847421ccb64c1f0d16fc937f09596e
Eduardo Cordeiro Maia Monteiro <edufacfito@gmail.com>
2022-07-27 21:53:50 -0300

    Servidor local e Nodemon

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const http = require("http");
 const port = 3000;
 
+const rotas = {
+    '/' : 'Curso de Node',
+    '/livros': 'Entrei na pag. de livros',
+    '/autores': 'Lista de autores',
+    '/sobre': 'Info sobre o projeto'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Contnt-type': 'text/plain'});
-    res.end('Curso de Node');
+    res.end(rotas[req.url]);
 });
 
 server.listen(port, () => {
-    console.log('Servidor escutando em http://localhost: ${port}');
+    console.log('Servidor escutando em http://localhost: ' + port);
 })
